2e1d144589449eadc29c47d2d0d62bbf2d151bc3
daniel pae <daniel pae>
2017-02-12 22:55:55 -0500

    it works, needs more tests, and to count solutions

--- 02Queens/QueensBoard.java
+++ 02Queens/QueensBoard.java
@@ -10,13 +10,13 @@ public class QueensBoard{
     }
 
     private boolean hSolve(int col){
-	if(col == 0){
+	if(col == board.length){
 	    return true;
 	}
-	for(int row; row < board.length; row++){
+	for(int row = 0; row < board.length; row++){
 	    if(board[col][row] == 0){
 		addQueen(col,row);
-		if(solveH(col + 1)){
+		if(hSolve(col + 1)){
 		    return true;
 		}removeQueen(col,row);
 	    }
@@ -24,13 +24,56 @@ public class QueensBoard{
     }
 
     public void addQueen(int col, int row){
-	board[col][row] == -1;
-	for(int i = 0; i < board.length; i++){
+       	for(int i = 0; i < board.length; i++){
 	    board[col][i]++;
 	    board[i][row]++;
-	    if(i < board.length){
-		board[i][row + col]
+	    if(row - col + i < board.length && row - col + i >= 0){
+		board[i][row - col + i]++;
 	    }
+	    if(row + col - i < board.length && row + col - i >= 0){
+		board[i][row + col - i]++;
+	    }
+	    board[col][row] = -1;
 	}
+    }
+
+    public void removeQueen(int col, int row){
+       	for(int i = 0; i < board.length; i++){
+	    board[col][i]--;
+	    board[i][row]--;
+	    if(row - col + i < board.length && row - col + i >= 0){
+		board[i][row - col + i]--;
+	    }
+	    if(row + col - i < board.length && row + col - i >= 0){
+		board[i][row + col - i]--;
+	    }
+	    board[col][row] = 0;
+	}
+    }
+
+    public String toString(){
+	String temp = "";
+	for(int r = 0; r < board.length; r++){
+	    for(int c = 0; c < board.length; c++){
+		temp = temp + board[c][r] + " ";
+	    }temp = temp + "\n";
+	}return temp;
+    }
+
+    public static void main(String[] args){
+	QueensBoard a = new QueensBoard(2);
+	a.addQueen(1,1);
+	System.out.println(a);
+	a.removeQueen(1,1);
+	System.out.println(a);
+	System.out.println(a.solve());
+	System.out.println(a);
+	QueensBoard b = new QueensBoard(8);
+	b.addQueen(1,1);
+	System.out.println(b);
+	b.removeQueen(1,1);
+	System.out.println(b);
+	System.out.println(b.solve());
+	System.out.println(b);
     }
 }
