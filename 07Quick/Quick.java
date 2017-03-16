df787f6a2c112d3e67bf4b121322d67eb2d156b5
DanielPae <danielpae104@gmail.com>
2017-03-16 15:30:39 -0400

    better, but not yet good

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,35 +4,33 @@ public class Quick{
     public static int[] partition(int[] data, int start, int end){
 	Random rn = new Random();
 	int piviot = rn.nextInt(end - start) + start;
-	int lowest = start;
 	int highest = end;
 	int temp = 0;
 	System.out.println(piviot + "");
 	for(int i = start; i < end; i++){
-	    if(piviot != i){
-		if(data[i] < data[piviot]){
-		    temp = data[lowest];
-		    data[lowest] = data[i];
-		    data[i] = temp;
-		    lowest++;
-		    i--;
-		}else if(data[i] > data[piviot]){
-		    temp = data[highest];
-		    data[highest] = data[i];
-		    data[i] = temp;
-		    highest--;
-		    i--;
-		}
-	    }
-	    for(int a : data){
-		System.out.print(a + " ");
-	    }System.out.print("\n");
-	}return data;
+	    if(i == highest) i = end;
+	    if(i == piviot) i++;
+	    if(highest == piviot) highest--;
+	    while(i < highest && data[i] < data[piviot]){
+		i++;
+	    }while(i < highest && data[i] > data[piviot]){
+		highest--;
+	    }temp = data[highest];
+	    data[highest] = data[i];
+	    data[i] = temp;
+	}temp = data[highest];
+	data[highest] = data[piviot];
+	data[piviot] = temp;
+	return data;
+    }
+
+    public static int quickSelect(int[] data, int k){
+	data = partition(data,0,data.length - 1);
+	return data[k];
     }
 
     public static void main(String[] args){
-	int[] a = {2,10,15,23,0,1};
-	System.out.println(a.length);
+	int[] a = {4,2,1,3,5};
 	int[] b = partition(a,0,a.length - 1);
 	for(int c : b){
 	    System.out.print(c + " ");
