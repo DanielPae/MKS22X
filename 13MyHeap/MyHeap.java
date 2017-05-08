dcce6e0a046efabf24f7c4833800300aa53d8307
daniel pae <daniel pae>
2017-05-08 00:33:45 -0400

    Still very buggy, needs more testing

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -17,8 +17,21 @@ public class MyHeap{
 	if(size == heap.length){
 	    reSize();
 	}heap[size] = s;
+	pushUp(size);
     }
 
+    public String remove(){
+	String ans = heap[1];
+	heap[1] = heap[size];
+	size--;
+	pushDown(1);
+	return ans;
+    }
+
+    public String peak(){
+	return heap[1];
+    }
+    
     private void reSize(){
 	String[] ans = new String[size * 2];
 	for(int i = 1; i <= size; i++){
@@ -27,39 +40,42 @@ public class MyHeap{
     }
 
     private int pushUp(int index){
-	String temp = heap[index];
-	heap[index] = heap[index / 2];
-	heap[index / 2] = temp;
-	index = index / 2;
-	System.out.println(heap[index / 2]);
-	if(index != 1 && heap[index / 2].compareTo(heap[index]) * constant > 0){
-	    return pushUp(index);
+	while(index != 1 && heap[index / 2].compareTo(heap[index]) * constant > 0){
+	    String temp = heap[index];
+	    heap[index] = heap[index / 2];
+	    heap[index / 2] = temp;
+	    index = index / 2;
+	    for(String n : heap){
+		System.out.print(n);
+	    }System.out.println("");
 	}return index;
     }
 
     private int pushDown(int index){
 	String temp = heap[index];
-	if(heap[index * 2].compareTo(heap[index * 2 + 1]) * constant < 0){
-	    heap[index] = heap[index * 2 + 1];
-	    heap[index * 2 + 1] = temp;
-	    return index * 2 + 1;
-	}else{
+	if(size > index * 2 
+	   && heap[index].compareTo(heap[index * 2]) * constant > 0
+	   && (size < index * 2 + 1 ||
+	       heap[index * 2].compareTo(heap[index * 2 + 1]) * constant > 0)){
 	    heap[index] = heap[index * 2];
 	    heap[index * 2] = temp;
-	    return index * 2;
-	}
-	if(index != size && 
-	   heap[index * 2].compareTo
+	    return pushDown(index * 2);
+	}else if(size > index * 2 + 1
+		 && heap[index].compareTo(heap[index * 2 + 1]) * constant > 0){
+	    heap[index] = heap[index * 2 + 1];
+	    heap[index * 2 + 1] = temp;
+	    return pushDown(index * 2 + 1);
+	}return index;
     }
 
     public static void main(String args[]){
 	MyHeap a = new MyHeap();
-	for(int i = 0; i < 10; i++){
-	    a.add(i + " ");
-	}a.pushUp(4);
-	a.pushDown(1);
-	for(String n : a.heap){
-	    System.out.print(n);
+	String b = "abcdefghijkmlnop";
+	for(int i = 11; i >= 0; i--){
+	    a.add(" " + b.charAt(i) + i + " ");
+	}System.out.println("");
+        for(int i = 11; i >= 0; i--){
+	    System.out.print(a.remove() + " ");
 	}
     }
 }
